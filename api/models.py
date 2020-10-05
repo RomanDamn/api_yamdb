5b7ac888eda43dae9fada49255c092d58deeb396
Maksim Prokopev <jazzelastic@gmail.com>
2020-10-05 11:16:22 +0300

    Fixed import order

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
 from api.utils import current_year, max_value_current_year
 
 
